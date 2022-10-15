c5cd09931264d54fc6510159d4f0c1dc06bc23a6
GlebKivilevich <gleb1106f@gmail.com>
2022-10-15 20:47:38 +0400

    typeset-main-page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from"./img/logo.png";
+import cart from "./img/cart.svg"
+import user from "./img/user.svg"
+import plus from "./img/plus.svg";
+import sneakers from "./img/sneakers/sneakerF.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} hight={40} src={logo}/>
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
+        </div>        
+        <ul className="headerRight d-flex ">
+          <li className="mr-30">
+            <img width={18} hight={18} src={cart} /> 
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={18} hight={18} src={user}/>
+          </li>
+        </ul>
       </header>
+      <div className="content p-40">
+        <h1 className="mb-40">Все кроссовки</h1>
+          <div className="card">
+            <img width={133} hight={133} src={sneakers} alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена: </span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} hight={11} src={plus} alt=""/>
+              </button>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
